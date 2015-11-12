551da5e0406906c0d42d717dd415f1e2e32a7d88
vbalan <victor.balan93@gmail.com>
2015-11-12 16:39:55 +0200

    Add javadocs

---
+++ src/main/java/uno/cod/battle/client/exceptions/BehaviourCode.java
@@ -0,0 +1,19 @@
+package uno.cod.battle.client.exceptions;
+
+/**
+ * Created by vbalan on 11/12/2015.
+ */
+public enum BehaviourCode {
+    STUN,
+    SUDO,
+    RW_LOCK_SPELL,
+    RW_LOCK_MOVE,
+    INVINCIBLE,
+    SILENCE,
+    DISARM,
+    FREEZE,
+    CORRUPT_IP_PACKET,
+    HEALTHFOUNTAIN,
+    RATE_LIMIT,
+    GLOBAL_COOLDOWN;
+}
